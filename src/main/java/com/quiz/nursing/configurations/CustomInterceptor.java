5a47fed4ae042f956fc152f08f2e4f98248b4677
S Vyas <dev.smitvyas@gmail.com>
2023-06-16 13:13:52 +0530

    included custom 404 page, removed white label error page, included component annotation for interceptor

--- src/main/java/com/quiz/nursing/configurations/CustomInterceptor.java
+++ src/main/java/com/quiz/nursing/configurations/CustomInterceptor.java
@@ -2,25 +2,27 @@ package com.quiz.nursing.configurations;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+@Component
 public class CustomInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String version = request.getHeader("version");
         String timestamp = request.getHeader("timestamp");
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
 
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
     }
 }
